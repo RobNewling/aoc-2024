15ad14c8327de5a7a687a6a506eabbfb1963d4a1
Rob Newling <rnewling@gmail.com>
2024-12-01 10:24:09 -0500

    test two passing

--- aoc-2024-tests/Day01Tests.cs
+++ aoc-2024-tests/Day01Tests.cs
@@ -18,4 +18,19 @@ public class Day01Tests
         // Assert
         result.Length.Should().BeGreaterThan(0);
     }
+
+    [Test]
+    public void Can_Load_Into_Two_Lists()
+    {
+        // Arrange
+        var day01 = new Day01();
+        var inputData = day01.ReadInputFile("Day01.txt");
+        
+        // Act
+        day01.LoadIntoLists(inputData);
+        
+        // Assert
+        day01.GetListOneLength().Should().BeGreaterThan(0);
+        day01.GetListTwoLength().Should().BeGreaterThan(0);
+    }
 }
--- aoc-2024/Day01.cs
+++ aoc-2024/Day01.cs
@@ -3,8 +3,41 @@ namespace aoc_2024;
 public class Day01
 {
     private const string InputFolderPath = "/Users/robnewling/Projects/aoc-2024/aoc-2024/Inputs";
-    public string ReadInputFile(string fileName)
+    private List<int> _listOne  = new ();
+    private List<int> _listTwo  = new ();
+    
+    public string[] ReadInputFile(string fileName)
     {
-        return File.ReadAllText(InputFolderPath + "/" + fileName);
+        return File.ReadAllLines(InputFolderPath + "/" + fileName);
+    }
+
+
+    public void LoadIntoLists(string[] inputData)
+    {
+        foreach (string line in inputData)
+        {
+            AddSplitLineToLists(SplitLine(line));
+        }
+    }
+
+    private string[] SplitLine(string line)
+    {
+        return line.Split("   ");
+    }
+
+    private void AddSplitLineToLists(string[] splitLine)
+    {
+        _listOne.Add(int.Parse(splitLine[0]));
+        _listTwo.Add(int.Parse(splitLine[1]));
+    }
+
+    public int GetListOneLength()
+    {
+        return _listOne.Count;
+    }
+
+    public int GetListTwoLength()
+    {
+        return _listTwo.Count;
     }
 }
