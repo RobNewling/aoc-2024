f010d9b6eff2fb08e0b00d95a471378bf90ccca7
Rob Newling <rnewling@gmail.com>
2024-12-03 00:05:43 -0500

    all tests passing

--- aoc-2024-tests/Day02Tests.cs
+++ aoc-2024-tests/Day02Tests.cs
@@ -18,9 +18,9 @@ public class Day02Tests
         // Assert
         result.Length.Should().BeGreaterThan(0);
     }
-
+    
     [Test]
-    public void Next_Number_Is_Higher()
+    public void Loading_Reports_Has_Length()
     {
         // Arrange
         var day02 = new Day02();
@@ -31,6 +31,129 @@ public class Day02Tests
         
         // Assert
         day02.GetReports().Length.Should().BeGreaterThan(0);
+    }
 
+    [Test]
+    public void Report_String_Split_Into_Int_Array()
+    {
+        // Arrange
+        var day02 = new Day02();
+        var reports = day02.ReadInputFile("Day02_Part1_Sample.txt");
+        day02.LoadReports(reports);
+        
+        // Act
+        var result = day02.SplitIntoIntArray(day02.GetReports()[0]);
+        
+        // Assert
+        result.Length.Should().Be(5);
+    }
+    
+    [Test]
+    public void Next_Number_Is_Higher_Returns_False()
+    {
+        // Arrange
+        var day02 = new Day02();
+        var reports = day02.ReadInputFile("Day02_Part1_Sample.txt");
+        day02.LoadReports(reports);
+
+        // Act
+        var result = day02.IsNextNumberHigher(0);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+    
+    [Test]
+    public void Pair_Is_In_Safe_Range()
+    {
+        // Arrange
+        var day02 = new Day02();
+        var reports = day02.ReadInputFile("Day02_Part1_Sample.txt");
+        day02.LoadReports(reports);
+        var intArray = day02.SplitIntoIntArray(reports[0]);
+        
+        //Act
+        var result = day02.IsPairInSafeRange(intArray[0], intArray[1]);
+        
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Test]
+    public void Check_Each_Number_In_Report()
+    {
+        // Arrange
+        var day02 = new Day02();
+        var reports = day02.ReadInputFile("Day02_Part1_Sample.txt");
+        day02.LoadReports(reports);
+        var intArray = day02.SplitIntoIntArray(reports[3]);
+        
+        //Act
+        var result = day02.CheckEachNumberInReportIsSafe(intArray);
+        
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Test]
+    public void Report_Has_Numbers_Within_Three_Of_Each_Other()
+    {
+        // Arrange
+        var day02 = new Day02();
+        var reports = day02.ReadInputFile("Day02_Part1_Sample.txt");
+        day02.LoadReports(reports);
+        
+        //Act
+        var result = day02.ReportIsSafe(day02.GetReports()[0]);
+        
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Test]
+    public void Number_Of_Safe_Reports()
+    {
+        // Arrange
+        var day02 = new Day02();
+        var reports = day02.ReadInputFile("Day02_Part1_Sample.txt");
+        day02.LoadReports(reports);
+        
+        //Act
+        var result = day02.NumberOfSafeReports(reports);
+        
+        // Assert
+        result.Should().Be(2);
+    }
+    
+    [Test]
+    public void Are_All_Increasing()
+    {
+        // Arrange
+        var day02 = new Day02();
+        var reports = day02.ReadInputFile("Day02_Part1_Sample.txt");
+        day02.LoadReports(reports);
+        var intArray = day02.SplitIntoIntArray(reports[1]);
+        
+        //Act
+        var result = day02.ReportIsAccending(intArray);
+        
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Test]
+    public void Are_All_Decreasing()
+    {
+        // Arrange
+        var day02 = new Day02();
+        var reports = day02.ReadInputFile("Day02_Part1_Sample.txt");
+        day02.LoadReports(reports);
+        var intArray = day02.SplitIntoIntArray(reports[0]);
+        
+        //Act
+        var result = day02.ReportIsDecending(intArray);
+        
+        // Assert
+        result.Should().BeTrue();
     }
 }
--- aoc-2024/Day02.cs
+++ aoc-2024/Day02.cs
@@ -19,4 +19,68 @@ public class Day02
     {
         return _reports;
     }
+
+    public bool IsNextNumberHigher(int index)
+    {
+        var report = SplitIntoIntArray(_reports[0]);
+        return report[index] < report[index + 1];
+    }
+
+    public int[] SplitIntoIntArray(string report)
+    {
+        return report.Split(' ').Select(int.Parse).ToArray();
+    }
+
+    public bool ReportIsSafe(string report)
+    {
+        var intArray = SplitIntoIntArray(report);
+        return CheckEachNumberInReportIsSafe(intArray) && ReportIsAccending(intArray) ^ ReportIsDecending(intArray);
+    }
+
+    public bool CheckEachNumberInReportIsSafe(int[] report)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (IsPairInSafeRange(report[i], report[i + 1]) == false)
+            {
+                return false;
+            };
+        }
+        return true;
+    }
+
+    public bool ReportIsAccending(int[] report)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (report[i] > report[i + 1])
+            {
+                return false;
+            };
+        }
+        return true;
+    }
+    
+    public bool ReportIsDecending(int[] report)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (report[i] < report[i + 1])
+            {
+                return false;
+            };
+        }
+        return true;
+    }
+    
+
+    public bool IsPairInSafeRange(int first, int second)
+    {
+        return first != second && Math.Abs(first - second) <= 3;
+    }
+
+    public int NumberOfSafeReports(string[] reports)
+    {
+        return reports.Count(n => ReportIsSafe(n));
+    }
 }
